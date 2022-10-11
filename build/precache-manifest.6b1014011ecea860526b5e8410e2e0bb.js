f75b351510d17c5f1eb47c9954e388f4a2cb0201
Asad Ansari <115532433+asadansari8840@users.noreply.github.com>
2022-10-11 19:52:09 +0530

    Add files via upload

---
+++ build/precache-manifest.6b1014011ecea860526b5e8410e2e0bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7996d9bef0b213ad7ac2",
+    "url": "/textUtilsAsad/static/js/main.7996d9be.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/textUtilsAsad/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "4caa4ed7ee1404eff48e",
+    "url": "/textUtilsAsad/static/js/runtime~main.4caa4ed7.js"
+  },
+  {
+    "revision": "c7295038590dcceef30de8a1de30dd0e",
+    "url": "/textUtilsAsad/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/textUtilsAsad/precache-manifest.6b1014011ecea860526b5e8410e2e0bb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/textUtilsAsad/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
